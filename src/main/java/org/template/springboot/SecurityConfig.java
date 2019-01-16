53c59ae4d38de147ba21d6140da34bc30525a140
Maxwell1987 <maxwell1987@foxmail.com>
2019-01-16 15:18:11 +0800

    Init.

---
+++ src/main/java/org/template/springboot/SecurityConfig.java
@@ -0,0 +1,17 @@
+package org.template.springboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+				.antMatchers("/**")
+				.antMatchers("/static/**");
+	}
+
+}
